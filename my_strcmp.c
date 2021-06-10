1c7e5e597243c6358f244b54910352197bf14590
Thays Ribeiro <thaysribeiros@gmail.com>
2021-06-10 15:09:18 -0700

    Create my_strcmp.c

---
+++ my_strcmp.c
@@ -0,0 +1,11 @@
+#include "my_ls.h"
+
+int my_strcmp(char* str1, char* str2)
+{
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i])
+    {
+        i += 1;
+    }
+    return (str1[i] - str2[i]);
+}
